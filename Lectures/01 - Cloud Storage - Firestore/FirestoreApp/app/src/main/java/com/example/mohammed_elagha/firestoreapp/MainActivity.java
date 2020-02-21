a5d5481ed1efb50c5d75341f9a649925180d7e8b
Mohammed <dev.magha@gmail.com>
2020-02-21 17:19:12 +0200

    Lecture: Firestore

---
+++ Lectures/01 - Cloud Storage - Firestore/FirestoreApp/app/src/main/java/com/example/mohammed_elagha/firestoreapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.mohammed_elagha.firestoreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        addData();
+//        readData();
+    }
+
+    private void addData () {
+        Map<String, String> user = new HashMap<>();
+        user.put("name", "Ahmed");
+        user.put("email", "aloosh@gmail.com");
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+
+                    }
+                });
+    }
+
+    private void readData () {
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("db_data", document.getId() + " => " + document.getData().get("name"));
+                            }
+                        }
+                    }
+                });
+    }
+}
