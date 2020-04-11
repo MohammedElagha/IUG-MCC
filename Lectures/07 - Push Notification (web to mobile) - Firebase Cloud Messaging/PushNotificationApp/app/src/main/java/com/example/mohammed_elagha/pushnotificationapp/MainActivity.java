956143b428341a9161bdf6859e96176119551d4a
Mohammed <dev.magha@gmail.com>
2020-04-11 13:07:37 +0300

    Push Notification Lecture

---
+++ Lectures/07 - Push Notification (web to mobile) - Firebase Cloud Messaging/PushNotificationApp/app/src/main/java/com/example/mohammed_elagha/pushnotificationapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mohammed_elagha.pushnotificationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getRegistrationToken();
+    }
+
+    public void getRegistrationToken () {
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (task.isSuccessful()) {
+                            String token = task.getResult().getToken();
+                            Log.d("REG_TOKEN", token);
+                        }
+                    }
+                });
+    }
+}
---
+++ Lectures/07 - Push Notification (web to mobile) - Firebase Cloud Messaging/PushNotificationApp/app/src/main/java/com/example/mohammed_elagha/pushnotificationapp/MyFirebaseMessagingService.java
@@ -0,0 +1,32 @@
+package com.example.mohammed_elagha.pushnotificationapp;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.core.app.NotificationCompat;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(message);
+
+        Log.d("NOTIFICATION_TITLE", message.getNotification().getTitle());
+        Log.d("NOTIFICATION_BODY", message.getNotification().getBody());
+
+    }
+}
